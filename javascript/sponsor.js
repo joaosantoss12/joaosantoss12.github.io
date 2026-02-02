6b5f182652e849e2817365e633bcb158d67a3533
JoaoDuarte-Haupper <joao.duarte@haupper.pt>
2026-02-02 20:42:49 +0000

    u

--- javascript/sponsor.js
+++ javascript/sponsor.js
@@ -155,7 +155,12 @@ function openInfoPanel(button) {
   case "btnwinwin":
   sponsorText = "🎰 Discover Fair Wins at WinWin Casino! 🌟💰<br><br>Join WinWin and experience a world of top-tier slots, live casino action, and sports betting excitement! New players get a <b>100% Welcome Bonus or 150 Free Spins</b> on their first deposit!<br><br>🔥 <b>Thousands of Casino Games & Sports Events</b><br>💸 <b>Fast Withdrawals & Crypto-Friendly</b><br>🎁 <b>Daily Promotions & VIP Rewards</b><br>⚡ <b>24/7 Support & Secure Transactions</b><br><br>Don’t miss out—<b>sign up at WinWin now and start winning big!</b> 🎲🏆";
   break;
-  }
+
+  case "btnamonbet":
+
+  sponsorText = "🎰 Discover Fair Wins at AmonBet Casino! 🌟💰<br><br>Join AmonBet and experience a world of top-tier slots, live casino action, and sports betting excitement! New players get a <b>100% Welcome Bonus or 150 Free Spins</b> on their first deposit!<br><br>🔥 <b>Thousands of Casino Games & Sports Events</b><br>💸 <b>Fast Withdrawals & Crypto-Friendly</b><br>🎁 <b>Daily Promotions & VIP Rewards</b><br>⚡ <b>24/7 Support & Secure Transactions</b><br><br>Don’t miss out—<b>sign up at AmonBet now and start winning big!</b> 🎲🏆";
+    break;
+}
   
 
   // Set the image and text in the modal
