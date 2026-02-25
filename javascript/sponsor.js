d8270812342541aacc21241c6ceac2408b9b41c5
joaosantoss12 <souoduda2002@gmail.com>
2026-02-25 12:30:47 +0000

    ezz

--- javascript/sponsor.js
+++ javascript/sponsor.js
@@ -163,8 +163,11 @@ function openInfoPanel(button) {
   case "btnbetpanda":
     sponsorText = "🎰 Discover Fair Wins at BetPanda Casino! 🌟💰<br><br>Join BetPanda and experience a world of top-tier slots, live casino action, and sports betting excitement! New players get a <b>100% Welcome Bonus or 150 Free Spins</b> on their first deposit!<br><br>🔥 <b>Thousands of Casino Games & Sports Events</b><br>💸 <b>Fast Withdrawals & Crypto-Friendly</b><br>🎁 <b>Daily Promotions & VIP Rewards</b><br>⚡ <b>24/7 Support & Secure Transactions</b><br><br>Don’t miss out—<b>sign up at BetPanda now and start winning big!</b> 🎲🏆";
     break;
-  }
-  
+
+  case "btnezz":
+    sponsorText = "🎰 Discover Fair Wins at EZZ Casino! 🌟💰<br><br>Join EZZ and experience a world of top-tier slots, live casino action, and sports betting excitement! New players get a <b>100% Welcome Bonus or 150 Free Spins</b> on their first deposit!<br><br>🔥 <b>Thousands of Casino Games & Sports Events</b><br>💸 <b>Fast Withdrawals & Crypto-Friendly</b><br>🎁 <b>Daily Promotions & VIP Rewards</b><br>⚡ <b>24/7 Support & Secure Transactions</b><br><br>Don’t miss out—<b>sign up at EZZ now and start winning big!</b> 🎲🏆";
+    break;
+}
 
   // Set the image and text in the modal
   document.getElementById("modalImage").src = sponsorImage;
